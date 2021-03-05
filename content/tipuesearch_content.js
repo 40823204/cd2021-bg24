0ba72b7cb00540ced0c8c01b0404d91334bac1e9
40823204 <40823204@gm.nfu.edu.tw>
2021-03-05 15:36:03 +0800

    add works

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '分組倉儲： https://github.com/40823204/stage1-bg24 \n 組員：40823204、40823220 \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '分組倉儲： https://github.com/40823204/stage1-bg24 \n 組員：40823204、40823220 \n', 'tags': '', 'url': 'About.html'}, {'title': 'Works', 'text': '\n \n', 'tags': '', 'url': 'Works.html'}]};
